e40a04a0773d1770d6af5989ddc25dd3c2624c94
kristenzhang <kzhang@bellese.io>
2021-08-10 16:01:12 -0400

    my first spring boot app :)

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,56 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "123456789");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Number of Authors: " + authorRepository.count());
+
+
+        Publisher noble = new Publisher("Noble", "address line 1", "Silver Spring", "MD", "21045");
+        noble.getBooks().add(ddd);
+        noble.getBooks().add(noEJB);
+        ddd.setPublisher(noble);
+        noEJB.setPublisher(noble);
+        publisherRepository.save(noble);
+
+        System.out.println("Number of books for publisher: " + noble.getBooks().size());
+    }
+}
